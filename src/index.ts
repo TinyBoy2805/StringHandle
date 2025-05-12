5925ccfe575a0c3ca169a3e4a1a26a483967730b
HoangPhuc6705 <tonguyenhoangphuc6705@gmail.com>
2025-05-12 21:53:51 +0700

    update npm and rewrite it using typescript fuck js

---
+++ src/index.ts
@@ -0,0 +1,66 @@
+function removeDigits(s: string): string {
+  let temp: string = "";
+  s = s.trim();
+  for (let i = 0; i < s.length; ++i) {
+    if (!/[0-9]/.test(s[i])) {
+      temp += s[i];
+    }
+  }
+  return "";
+}
+
+function insertDot(s: string): string {
+  let ans: string = '';
+  s = s.trim();
+  for (let i = 0; i < s.length; i++) {
+    ans += s[i];
+    if ((i + 1) % 3 === 0 && i < s.length - 1) {
+      ans += '.';
+    }
+  }
+  return ans;
+}
+
+
+// function removeNumberCharacter(s) {
+//   let ans = '';
+//   s = s.trim();
+//   for (let i = 0; i < s.length; i++) {
+//     if (!/[0-9]/.test(s[i])) {
+//       ans += s[i];
+//     }
+//   }
+//   return ans;
+// }
+
+function removeUpperCaseCharacter(s: string): string {
+  let ans: string = '';
+  s = s.trim();
+  for (let i = 0; i < s.length; i++) {
+    if (!/[A-Z]/.test(s[i])) {
+      ans += s[i];
+    }
+  }
+  return ans;
+}
+
+function removeLowerCaseCharacter(s: string): string {
+  let ans: string = '';
+  s = s.trim();
+  for (let i = 0; i < s.length; i++) {
+    if (!/[a-z]/.test(s[i])) {
+      ans += s[i];
+    }
+  }
+  return ans;
+}
+
+
+// module.exports = { addDot, removeNumberCharacter, removeUpperCaseCharacter,  removeLowerCaseCharacter}
+
+module.exports = {
+  insertDot,
+  removeDigits,
+  removeUpperCaseCharacter,
+  removeLowerCaseCharacter
+}
