86c60202a473f92374849ef8eb311956b10c1abd
TinyBoy <ntp22880055@gmail.com>
2025-05-12 16:05:16 +0700

    second upload

--- index.js
+++ index.js
@@ -2,17 +2,51 @@
 
 function addDot(s) {
   let ans = '';
-  s = s.trim(); 
-  for (let i = 0; i < s.length; i++) 
-    {
-    ans += s[i]; 
-    if ((i + 1) % 3 === 0 && i < s.length - 1) 
-    { 
+  s = s.trim();
+  for (let i = 0; i < s.length; i++) {
+    ans += s[i];
+    if ((i + 1) % 3 === 0 && i < s.length - 1) {
       ans += '.';
     }
-    }
+  }
   return ans;
 }
 
-module.exports = { addDot }
+
+function removeNumberCharacter(s) {
+  let ans = '';
+  s = s.trim();
+  for (let i = 0; i < s.length; i++) {
+    if (!/[0-9]/.test(s[i])) {
+      ans += s[i];
+    }
+  }
+  return ans;
+}
+
+function removeUpperCaseCharacter(s) {
+  let ans = '';
+  s = s.trim();
+  for (let i = 0; i < s.length; i++) {
+    if (!/[A-Z]/.test(s[i])) {
+      ans += s[i];
+    }
+  }
+  return ans;
+}
+
+function removeLowerCaseCharacter(s) {
+  let ans = '';
+  s = s.trim();
+  for (let i = 0; i < s.length; i++) {
+    if (!/[a-z]/.test(s[i])) {
+      ans += s[i];
+    }
+  }
+  return ans;
+}
+
+
+
+module.exports = { addDot, removeNumberCharacter, removeUpperCaseCharacter,  removeLowerCaseCharacter}
 
